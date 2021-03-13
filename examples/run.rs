106f4fb65c6d352998d2d4325bfca3c5304c3cf3
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-03-12 22:20:11 -0800

    Add image bytes to serialized pokemon added run test

---
+++ examples/run.rs
@@ -0,0 +1,9 @@
+use std::time::Instant;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Building dex...");
+    let start = Instant::now();
+    dex_builder::build_dex("pokemon", "moves", "textures", "dex.bin")?;
+    println!("Finished in {}ms!", start.elapsed().as_millis());
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use std::fs::File;
+use std::io::Read;
 use std::io::Write;
+use std::path::Path;
 
-pub fn build_dex(pokemon_dir: &str, move_dir: &str, save_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+use firecore_pokedex::pokemon::Pokemon;
+use firecore_pokedex::serialized::SerializedPokemon;
+
+pub fn build_dex(pokemon_dir: &str, move_dir: &str, textures_dir: &str, save_file: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut file = File::create(save_file)?;
 
@@ -13,8 +18,20 @@ pub fn build_dex(pokemon_dir: &str, move_dir: &str, save_file: &str) -> Result<(
             if let Ok(entry) = entry {
                 let path = entry.path();
                 if let Ok(data) = std::fs::read_to_string(path) {
-                    if let Ok(pokemon_entry) = toml::from_str(&data) {
-                        pokemon.push(pokemon_entry);
+                    let result: Result<Pokemon, toml::de::Error> = toml::from_str(&data);
+                    if let Ok(pokemon_entry) = result {
+                        let name = pokemon_entry.data.name.to_ascii_lowercase();
+                        let mut front_png = Vec::new();
+                        let mut back_png = Vec::new();
+                        let base = Path::new(textures_dir).join("normal");
+                        File::open(base.join("front").join(name.clone() + ".png"))?.read_to_end(&mut front_png)?;
+                        File::open(base.join("back").join(name + ".png"))?.read_to_end(&mut back_png)?;
+                        pokemon.push(SerializedPokemon {
+                            pokemon: pokemon_entry,
+                            cry_ogg: Vec::new(),
+                            front_png,
+                            back_png,
+                        });
                     }
                 }
             }
@@ -36,7 +53,7 @@ pub fn build_dex(pokemon_dir: &str, move_dir: &str, save_file: &str) -> Result<(
 
     file.write_all(
         &bincode::serialize(
-            &firecore_pokedex::SerializedDex {
+            &firecore_pokedex::serialized::SerializedDex {
                 pokemon,
                 moves
             }
